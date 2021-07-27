5ec9abc3703d302a7a1bab490e3f5f69c748eedd
Letícia Castro <aicitelks@gmail.com>
2021-07-27 17:41:54 -0300

    Requisito 2: OK

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default class SearchBar extends React.Component {
   render() {
     return(
-      <div>
-        SearchBar
-      </div>
+      <form data-testid="search-bar-form">
+
+      </form>
     );
   }
 }
