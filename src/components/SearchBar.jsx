27d37a4487bb882235e9daaf9368e082c6d7f4bc
Letícia Castro <aicitelks@gmail.com>
2021-07-27 18:15:32 -0300

    Requisito 5: OK

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -36,6 +36,22 @@ export default class SearchBar extends React.Component {
           />
         </label>
 
+        <label data-testid="select-input-label">
+          Filtrar por gênero:
+          <select
+            data-testid="select-input"
+            value={ selectedGenre } 
+            onChange={ onSelectedGenreChange }
+            name="" 
+            id="" 
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+
       </form>
     );
   }
