473432545e7019e9a52885579b32149ed61c9516
Letícia Castro <aicitelks@gmail.com>
2021-07-27 18:09:52 -0300

    Requisito 4: OK

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,10 +13,27 @@ export default class SearchBar extends React.Component {
 
     return(
       <form data-testid="search-bar-form">
-        
+
         <label data-testid="text-input-label" htmlFor="">
           Inclui o texto:
-          <input data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
+          <input 
+            data-testid="text-input" 
+            type="text" 
+            value={ searchText } 
+            onChange={ onSearchTextChange } 
+          />
+        </label>
+
+        <label data-testid="checkbox-input-label" htmlFor="">
+          Mostrar somente favoritos:
+          <input 
+            data-testid="checkbox-input" 
+            type="checkbox" 
+            checked={ bookmarkedOnly } 
+            onChange={ onBookmarkedChange } 
+            name="" 
+            id="" 
+          />
         </label>
 
       </form>
