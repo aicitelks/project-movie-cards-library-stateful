8bf2504241d9930ccf6a0f411f65aa4bf6dd30cd
Letícia Castro <aicitelks@gmail.com>
2021-07-27 17:22:45 -0300

    <SearchBar /> criado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+
 import './App.css';
 
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <SearchBar />
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement SearchBar component here
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  render() {
+    return(
+      <div>
+        SearchBar
+      </div>
+    );
+  }
+}
+
