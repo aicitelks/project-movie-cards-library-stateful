b48cba84a8bdc70fa6aead0b86f71297eb1bfd6c
Letícia Castro <aicitelks@gmail.com>
2021-07-28 15:54:06 -0300

    Requisito 8: OK

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,14 +13,34 @@ export default class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
   
+  handleChange(event) {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
   render() {
-    const INITIAL_STATE = this.state;
+    const {subtitle, title, imagePath, storyline, rating, genre} = this.state;
 
     return (
       <div>
-        <form data-testid="add-movie-form"></form>
+        <form data-testid="add-movie-form">
+          <div>
+            <label data-testid="title-input-label" htmlFor="input-title"> Título:
+              <input
+                id="input-title"
+                data-testid="title-input"
+                type="text"
+                value={ title }
+                onChange={ this.handleChange }
+              />
+            </label>
+          </div>
+        </form>
       </div>
     );
   }
